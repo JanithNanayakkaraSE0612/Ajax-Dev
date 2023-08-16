1cd3bbf130c8b0a466ac7ceea494eaf9a09845e0
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-08-16 15:55:46 +0530

    animation added

--- Ajax_Exersice_01/js/script.js
+++ Ajax_Exersice_01/js/script.js
@@ -1,1 +1,10 @@
 
+$(document).ready(function (){
+    $('main').load('content.html #home');
+
+    $('a').click(function (){
+        var ClickLink = $(this).attr('id');
+        alert(ClickLink);
+        $('main').load('content.html #'+ClickLink);
+    });
+})
