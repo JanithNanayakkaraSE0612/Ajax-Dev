66a2ea06803c8f8cd9284310c14ad52f8e0addc0
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-08-16 16:03:47 +0530

    Completed

--- Ajax_Exersice_01/js/script.js
+++ Ajax_Exersice_01/js/script.js
@@ -3,6 +3,9 @@ $(document).ready(function (){
     $('main').load('content.html #home');
 
     $('a').click(function (){
+        $('main').html(' <div class="loading">\n' +
+            '          <img src="img/Loading_icon.gif" alt="loading">\n' +
+            '      </div>');
         var ClickLink = $(this).attr('id');
         alert(ClickLink);
         $('main').load('content.html #'+ClickLink);
